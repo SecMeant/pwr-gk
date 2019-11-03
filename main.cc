2fefd273724dfb6123f051d576fe181a92d50b44
secmeant <secmeant@gmail.com>
2019-11-03 15:20:35 +0100

    Adding iterative sierpinski with random offsets

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include <GL/glut.h>
 #include <cstdlib>
 #include <ctime>
+#include <fmt/format.h>
 
 /*************************************************************************************/
 
@@ -22,6 +23,11 @@ constexpr Color green{0.0f, 1.0f, 0.0f};
 constexpr Color blue{0.0f, 0.0f, 1.0f};
 
 constexpr GLfloat RAND_MAXf = (double) RAND_MAX;
+
+Color RandomColor()
+{
+  return {rand() / RAND_MAXf, rand() / RAND_MAXf, rand() / RAND_MAXf};
+}
 
 void draw2Triangles()
 {
@@ -54,14 +60,35 @@ void draw3ColorTriangle()
   glEnd();
 }
 
+#define RANDOM_OFFSET ((static_cast<GLfloat>(rand() % 16) - 8.0f) / 32.0f)
 void DrawSquare(Square sq)
 {
   glColor3f(sq.color.r, sq.color.g, sq.color.b);
   glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(sq.midx - sq.rad, sq.midy - sq.rad);
-    glVertex2f(sq.midx - sq.rad, sq.midy + sq.rad);
-    glVertex2f(sq.midx + sq.rad, sq.midy - sq.rad);
-    glVertex2f(sq.midx + sq.rad, sq.midy + sq.rad);
+    glVertex2f(sq.midx - sq.rad + RANDOM_OFFSET, sq.midy - sq.rad + RANDOM_OFFSET);
+    glVertex2f(sq.midx - sq.rad + RANDOM_OFFSET, sq.midy + sq.rad + RANDOM_OFFSET);
+    glVertex2f(sq.midx + sq.rad + RANDOM_OFFSET, sq.midy - sq.rad + RANDOM_OFFSET);
+    glVertex2f(sq.midx + sq.rad + RANDOM_OFFSET, sq.midy + sq.rad + RANDOM_OFFSET);
+  glEnd();
+}
+
+#define RAINBOW_SWITCH \
+{ \
+  Color color = RandomColor(); \
+  glColor3f(color.r, color.g, color.b); \
+}
+
+void DrawPrettySquare(Square sq)
+{
+  glBegin(GL_TRIANGLE_STRIP);
+    RAINBOW_SWITCH
+    glVertex2f(sq.midx - sq.rad + RANDOM_OFFSET, sq.midy - sq.rad + RANDOM_OFFSET);
+    RAINBOW_SWITCH
+    glVertex2f(sq.midx - sq.rad + RANDOM_OFFSET, sq.midy + sq.rad + RANDOM_OFFSET);
+    RAINBOW_SWITCH
+    glVertex2f(sq.midx + sq.rad + RANDOM_OFFSET, sq.midy - sq.rad + RANDOM_OFFSET);
+    RAINBOW_SWITCH
+    glVertex2f(sq.midx + sq.rad + RANDOM_OFFSET, sq.midy + sq.rad + RANDOM_OFFSET);
   glEnd();
 }
 
@@ -70,11 +97,6 @@ void RemoveSquareMid(Square sq)
   sq.rad /= 3.0f;
   //sq.color = bgcolor;
   DrawSquare(sq);
-}
-
-Color RandomColor()
-{
-  return {rand() / RAND_MAXf, rand() / RAND_MAXf, rand() / RAND_MAXf};
 }
 
 void Sierpinski(Square sq, GLfloat limit = 5.0f)
@@ -114,15 +136,58 @@ void Sierpinski(Square sq, GLfloat limit = 5.0f)
   Sierpinski(sq9);
 }
 
+using uint = unsigned int;
+
+void SierpinskiIter(Square sqbase, uint level)
+{
+  Square sq = sqbase;
+  GLfloat off;
+
+  DrawPrettySquare(sqbase);
+
+  for (uint i = 0; i < level; ++i)
+  {
+    sq.rad /= 3;
+    off = sq.rad * 2;
+
+    GLfloat midx_start = sq.midx - off;
+    GLfloat midy_start = sq.midy + off;
+
+    sq.midx = midx_start;
+    sq.midy = midy_start;
+
+    //fmt::print("Main X:{} Y:{} off:{}\n", sqbase.midx, sqbase.midy, off);
+    //fmt::print("Small X:{} Y:{}\n", sq.midx, sq.midy);
+    //fmt::print("Stop at X:{} Y:{}\n\n", sqbase.midx + sqbase.rad, sqbase.midy - sqbase.rad);
+
+    while (sq.midy >= sqbase.midy - sqbase.rad)
+    {
+      while (sq.midx <= sqbase.midx + sqbase.rad)
+      {
+        RemoveSquareMid(sq);
+
+        //fmt::print("Small X:{} Y:{}\n", sq.midx, sq.midy);
+        sq.midx += off;
+      }
+      sq.midx = midx_start;
+      sq.midy -= off;
+    }
+    sq.midy = midy_start;
+
+    //fmt::print("\n\n=======\n\n");
+  }
+}
+
 void RenderScene(void)
 
 {
   glClear(GL_COLOR_BUFFER_BIT);
   // Czyszczenie okna aktualnym kolorem czyszczącym
 
-  Square sq{0.0f, 0.0f, 80.0f, RandomColor()};
-  DrawSquare(sq);
-  Sierpinski(sq, 0.01f);
+  Square sq{0.0f, 0.0f, 80.0f, bgcolor};
+  //DrawSquare(sq);
+  //Sierpinski(sq, 0.01f);
+  SierpinskiIter(sq, 5);
 
   glFlush();
   // Przekazanie poleceń rysujących do wykonania
@@ -216,6 +281,7 @@ int main(int argc, char **argv)
 {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+  glutInitWindowSize(1000, 1000);
   // Ustawienie trybu wyświetlania
   // GLUT_SINGLE - pojedynczy bufor wyświetlania
   // GLUT_RGBA - model kolorów RGB
