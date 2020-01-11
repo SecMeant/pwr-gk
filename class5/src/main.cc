04780e48b0f5c1bcd34a78d12c6e735750abadf9
secmeant <secmeant@gmail.com>
2020-01-11 10:11:43 +0100

    Class5 all works

--- class5/src/main.cc
+++ class5/src/main.cc
@@ -1,68 +1,373 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
-#include <cstdlib>
-#include <ctime>
+#include <cmath>
 #include <fmt/format.h>
+
+typedef float point3[3];
+
+static GLfloat viewer[] = { 10.0, 5.0, 10.0 };
+static GLfloat thetax = 0.0;
+static GLfloat thetay = 0.0;
+static GLfloat pix2anglex;
+static GLfloat pix2angley;
+
+int figure = 0;
+
+static GLint status = 0;
+
+static int x_pos_old = 0;
+static int delta_x = 0;
+
+static int y_pos_old = 0;
+static int delta_y = 0;
+
+static GLfloat scale = 1.0;
+
+static GLfloat offset_x = 0;
+static GLfloat offset_y = 0;
+
+static GLfloat R = 1.0;
+static GLfloat alpha = M_PI / 2.0;
+static GLfloat beta = 0;
+
+static GLfloat lookpoint_x = 0;
+static GLfloat lookpoint_y = 0;
+static GLfloat lookpoint_z = 0;
+
+constexpr float N = 512.0f;
+constexpr size_t uN = static_cast<size_t>(N);
+
+point3 eggpoints[uN][uN];
+point3 eggnorms[uN][uN]{};
+point3 eggcolo[uN][uN];
+
+GLfloat mat_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_shininess = { 20.0 };
+
+GLfloat light_position[] = { 2.5, 2.5, 10.0, 1.0 };
+GLfloat light_ambient[] = { 0.1, 0.1, 0.1, 1.0 };
+GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat att_constant = { 1.0 };
+GLfloat att_linear = { 0.05 };
+GLfloat att_quadratic = { 0.001 };
 
 struct Color
 {
   GLfloat r, g, b;
 };
 
-struct Square
-{
-  GLfloat midx, midy, rad;
-  Color color;
-};
-
-constexpr Color bgcolor{ 1.0f, 1.0f, 1.0f };
-constexpr Color red{ 1.0f, 0.0f, 0.0f };
-constexpr Color green{ 0.0f, 1.0f, 0.0f };
-constexpr Color blue{ 0.0f, 0.0f, 1.0f };
-
 constexpr GLfloat RAND_MAXf = (double)RAND_MAX;
 
-GLfloat light_position[] = { 0.0, 0.0, 10.0, 1.0 };
-GLfloat mat_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
-GLfloat mat_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-GLfloat mat_shininess = { 20.0 };
-GLfloat light_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
-GLfloat light_diffuse[] = { 1.0, 0.0, 1.0, 1.0 };
-GLfloat light_specular[] = { 1.0, 0.0, 0.0, 1.0 };
-GLfloat att_constant = { 1.0 };
-GLfloat att_linear = { 0.05 };
-GLfloat att_quadratic = { 0.001 };
-
-Color
-RandomColor()
+constexpr float
+egg_x(float u, float v)
 {
-  return { rand() / RAND_MAXf, rand() / RAND_MAXf, rand() / RAND_MAXf };
+  return (std::pow(u, 5.0f) * (-90.0f) + std::pow(u, 4.0f) * 225 +
+          std::pow(u, 3.0f) * (-270.0f) + std::pow(u, 2.0f) * 180.0f +
+          u * (-45.0f)) *
+         std::cos(M_PI * v);
 }
 
-using uint = unsigned int;
+constexpr float
+egg_y(float u, [[maybe_unused]] float v)
+{
+  return std::pow(u, 4.0f) * 160 + std::pow(u, 3.0f) * (-320.0f) +
+         std::pow(u, 2.0f) * 160.0f;
+}
+
+constexpr float
+egg_z(float u, float v)
+{
+  return (std::pow(u, 5.0f) * (-90.0f) + std::pow(u, 4.0f) * 225.0f +
+          std::pow(u, 3.0f) * (-270.0f) + std::pow(u, 2.0f) * 180 +
+          u * (-45.0f)) *
+         std::sin(M_PI * v);
+}
+
+constexpr float
+egg_normal_xu(float u, float v)
+{
+
+  return (-450.0f * std::pow(u, 4.0f) + 900.0f * std::pow(u, 3.0f) -
+          810.0f * std::pow(u, 2.0f) + 360.0f * u - 45) *
+         std::cos(M_PI * v);
+}
+
+constexpr float
+egg_normal_xv(float u, float v)
+{
+  return M_PI *
+         (90.0f * std::pow(u, 5.0f) - 225.0f * std::pow(u, 4.0f) +
+          270.0f * std::pow(u, 3.0f) - 180.0f * std::pow(u, 2.0f) + 45.0f * u) *
+         std::sin(M_PI * v);
+}
+
+constexpr float
+egg_normal_yu(float u, float v)
+{
+  return 640.0f * std::pow(u, 3.0f) - 960.0f * std::pow(u, 2.0f) + 320.0f * u;
+}
+
+constexpr float
+egg_normal_yv([[maybe_unused]] float u, [[maybe_unused]] float v)
+{
+  return 0;
+}
+
+constexpr float
+egg_normal_zu(float u, float v)
+{
+  return (-450.0f * std::pow(u, 4.0f) + 900.0f * std::pow(u, 3.0f) -
+          810.0f * std::pow(u, 2.0f) + 360.0f * u - 45) *
+         std::sin(M_PI * v);
+}
+
+constexpr float
+egg_normal_zv(float u, float v)
+{
+  return -M_PI *
+         (90.0f * std::pow(u, 5.0f) - 225.0f * std::pow(u, 4.0f) +
+          270.0f * std::pow(u, 3.0f) - 180.0f * std::pow(u, 2.0f) + 45.0f * u) *
+         std::cos(M_PI * v);
+}
+
+void
+egg_normal(float norm[3], float u, float v)
+{
+  u /= (N);
+  v /= (N);
+
+  auto xu = egg_normal_xu(u, v);
+  auto xv = egg_normal_xv(u, v);
+  auto yu = egg_normal_yu(u, v);
+  auto yv = egg_normal_yv(u, v);
+  auto zu = egg_normal_zu(u, v);
+  auto zv = egg_normal_zv(u, v);
+
+  norm[0] = yu * zv - zu * yv;
+  norm[1] = zu * xv - xu * zv;
+  norm[2] = xu * yv - yu * xv;
+
+  auto len =
+    std::sqrt(norm[0] * norm[0] + norm[1] * norm[1] + norm[2] * norm[2]);
+
+  if (u == 0 || u == N) {
+    norm[0] = 0;
+    norm[1] = -1;
+    norm[2] = 0;
+  } else if (u < 0.5f) {
+    norm[0] = (yu * zv - zu * yv) / len;
+    norm[1] = (zu * xv - xu * zv) / len;
+    norm[2] = (xu * yv - yu * xv) / len;
+  } else if (u > 0.5f) {
+    norm[0] = -1 * (yu * zv - zu * yv) / len;
+    norm[1] = -1 * (zu * xv - xu * zv) / len;
+    norm[2] = -1 * (xu * yv - yu * xv) / len;
+  } else {
+    norm[0] = 0;
+    norm[1] = 1;
+    norm[2] = 0;
+  }
+}
+
+void
+init_eggpoints()
+{
+  for (size_t j = 0; j < N; ++j) {
+    for (size_t i = 0; i < N; ++i) {
+      eggpoints[j][i][0] =
+        egg_x(static_cast<float>(j) / (N - 1), static_cast<float>(i) / (N - 1));
+
+      eggpoints[j][i][1] = egg_y(static_cast<float>(j) / (N - 1),
+                                 static_cast<float>(i) / (N - 1)) -
+                           5.0f;
+      eggpoints[j][i][2] =
+        egg_z(static_cast<float>(j) / (N - 1), static_cast<float>(i) / (N - 1));
+    }
+  }
+
+  for (size_t j = 0; j < N; ++j) {
+    for (size_t i = 0; i < N; ++i) {
+      Color c = { 0xd4, 0xaf, 0x37 };
+      eggcolo[j][i][0] = c.r;
+      eggcolo[j][i][1] = c.g;
+      eggcolo[j][i][2] = c.b;
+    }
+  }
+
+  for (size_t j = 0; j < N; ++j) {
+    for (size_t i = 0; i < N; ++i) {
+      egg_normal(eggnorms[j][i], j, i);
+    }
+  }
+}
+
+void
+Axes(void)
+{
+  point3 x_min = { -5.0, 0.0, 0.0 };
+  point3 x_max = { 5.0, 0.0, 0.0 };
+
+  point3 y_min = { 0.0, -5.0, 0.0 };
+  point3 y_max = { 0.0, 5.0, 0.0 };
+
+  point3 z_min = { 0.0, 0.0, -5.0 };
+  point3 z_max = { 0.0, 0.0, 5.0 };
+
+  glColor3f(1.0f, 0.0f, 0.0f); // kolor rysowania osi - czerwony
+  glBegin(GL_LINES);           // rysowanie osi x
+
+  glVertex3fv(x_min);
+  glVertex3fv(x_max);
+
+  glEnd();
+
+  glColor3f(0.0f, 1.0f, 0.0f); // kolor rysowania - zielony
+  glBegin(GL_LINES);           // rysowanie osi y
+
+  glVertex3fv(y_min);
+  glVertex3fv(y_max);
+
+  glEnd();
+
+  glColor3f(0.0f, 0.0f, 1.0f); // kolor rysowania - niebieski
+  glBegin(GL_LINES);           // rysowanie osi z
+
+  glVertex3fv(z_min);
+  glVertex3fv(z_max);
+
+  glEnd();
+}
+
+void
+draw_egg()
+{
+  glBegin(GL_TRIANGLES);
+  for (size_t j = 0; j < N - 1; ++j) {
+    for (size_t i = 0; i < N - 1; ++i) {
+      glNormal3fv(eggnorms[j][i]);
+      glTexCoord2f(0.0f, 0.0f);
+      glVertex3fv(eggpoints[j][i]);
+
+      glNormal3fv(eggnorms[j][i + 1]);
+      glTexCoord2f(0.75f, 0.0f);
+      glVertex3fv(eggpoints[j][i + 1]);
+
+      glNormal3fv(eggnorms[j + 1][i]);
+      glTexCoord2f(0.50f, 1.0f);
+      glVertex3fv(eggpoints[j + 1][i]);
+
+      glNormal3fv(eggnorms[j + 1][i]);
+      glTexCoord2f(0.0f, 0.0f);
+      glVertex3fv(eggpoints[j + 1][i]);
+
+      glNormal3fv(eggnorms[j + 1][i + 1]);
+      glTexCoord2f(0.75f, 0.0f);
+      glVertex3fv(eggpoints[j + 1][i + 1]);
+
+      glNormal3fv(eggnorms[j][i + 1]);
+      glTexCoord2f(0.50f, 1.0f);
+      glVertex3fv(eggpoints[j][i + 1]);
+    }
+  }
+  glEnd();
+}
+struct point3d
+{
+  GLfloat x, y, z;
+};
+
+void
+draw_tri(point3d p1, point3d p2, point3d p3)
+{
+  glBegin(GL_TRIANGLES);
+  glNormal3f(1.0f, 0.0f, 0.0f);
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(p1.x, p1.y, p1.z);
+
+  glNormal3f(1.0f, 0.0f, 0.0f);
+  glTexCoord2f(0.75f, 0.0f);
+  glVertex3f(p2.x, p2.y, p2.z);
+
+  glNormal3f(1.0f, 0.0f, 0.0f);
+  glTexCoord2f(0.50f, 1.0f);
+  glVertex3f(p3.x, p3.y, p3.z);
+  glEnd();
+}
+
+void
+draw_tri()
+{
+  draw_tri({ 0.0f, 0.0f }, { 5.0f, 0.0f }, { 0.0f, 5.0f });
+}
+
+void
+draw_pyra()
+{
+  draw_tri({ 0.0f, 0.0f, 0.0f }, { 5.0f, 0.0f, 0.0f }, { 2.5f, 4.0f, -2.5f });
+  draw_tri({ 0.0f, 0.0f, -5.0f }, { 0.0f, 0.0f, 0.0f }, { 2.5f, 4.0f, -2.5f });
+  draw_tri({ 5.0f, 0.0f, 0.0f }, { 5.0f, 0.0f, -5.0f }, { 2.5f, 4.0f, -2.5f });
+  draw_tri({ 5.0f, 0.0f, -5.0f }, { 0.0f, 0.0f, -5.0f }, { 2.5f, 4.0f, -2.5f });
+
+  // base
+  draw_tri({ 5.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, -5.0f });
+  draw_tri({ 5.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, -5.0f }, { 5.0f, 0.0f, -5.0f });
+}
 
 void
 RenderScene(void)
 {
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glBegin(GL_TRIANGLES);
-    glNormal3f(0.0f, 0.0f, 1.0f);
-    glTexCoord2f(0.25f, 0.25f);
-    glVertex2f(0.0f, 0.0f);
+  glLoadIdentity();
 
-    glNormal3f(0.0f, 0.0f, 1.0f);
-    glTexCoord2f(0.75f, 0.25f);
-    glVertex2f(50.0f, 0.0f);
+  auto vec = 1.0;
 
-    glNormal3f(0.0f, 0.0f, 1.0f);
-    glTexCoord2f(0.5f, 0.75f);
-    glVertex2f(0.0f, 50.0f);
-  glEnd();
+  gluLookAt(viewer[0],
+            viewer[1],
+            viewer[2],
+            lookpoint_x,
+            lookpoint_y,
+            lookpoint_z,
+            0.0,
+            vec,
+            0.0);
+
+  Axes();
+
+  if (status == 1) {
+    thetax += delta_x * pix2anglex;
+    thetay += delta_y * pix2angley;
+  }
+
+  glRotatef(thetax, 0.0, 1.0, 0.0);
+  glRotatef(thetay, 1.0, 0.0, 0.0);
+  glScalef(scale * 1.0, scale * 1.0, scale * 1.0);
+  glTranslatef(offset_x, offset_y, 0.0);
+
+  glColor3f(0.0f, 0.0f, 0.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glColor3f(1.0f, 1.0f, 1.0f);
+
+  switch (figure) {
+    case 0:
+      draw_egg();
+      break;
+    case 1:
+      draw_tri();
+      break;
+    case 2:
+      draw_pyra();
+      break;
+  }
 
   glFlush();
+
+  glutSwapBuffers();
 }
 
 GLbyte*
@@ -148,39 +453,58 @@ LoadTGAImage(const char* FileName,
   return pbitsperpixel;
 }
 
+struct texture
+{
+  GLint ImWidth, ImHeight, ImComponents;
+  GLenum ImFormat;
+  GLbyte* data;
+} t1, t2;
+int tex_active = 0;
+
+void
+set_texture(texture tex)
+{
+  glTexImage2D(GL_TEXTURE_2D,
+               0,
+               tex.ImComponents,
+               tex.ImWidth,
+               tex.ImHeight,
+               0,
+               tex.ImFormat,
+               GL_UNSIGNED_BYTE,
+               tex.data);
+}
+
 void
 MyInit(void)
-
 {
-  srand(time(0));
-
-  GLbyte* pBytes;
   GLint ImWidth, ImHeight, ImComponents;
   GLenum ImFormat;
 
   glEnable(GL_CULL_FACE);
 
-  pBytes =
-    LoadTGAImage("tekstury/D1_t.tga", &ImWidth, &ImHeight, &ImComponents, &ImFormat);
+  t1.data = LoadTGAImage("tekstury/D1_t.tga",
+                         &t1.ImWidth,
+                         &t1.ImHeight,
+                         &t1.ImComponents,
+                         &t1.ImFormat);
 
-  if (pBytes == NULL)
+  if (t1.data == NULL)
     fmt::print("Reading texture file failed\n");
 
-  glTexImage2D(GL_TEXTURE_2D,
-               0,
-               ImComponents,
-               ImWidth,
-               ImHeight,
-               0,
-               ImFormat,
-               GL_UNSIGNED_BYTE,
-               pBytes);
+  t2.data = LoadTGAImage("tekstury/yui.tga",
+                         &t2.ImWidth,
+                         &t2.ImHeight,
+                         &t2.ImComponents,
+                         &t2.ImFormat);
 
-  free(pBytes);
+  if (t2.data == NULL)
+    fmt::print("Reading texture file failed\n");
+
+  set_texture(t1);
 
   glEnable(GL_TEXTURE_2D);
   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
@@ -208,80 +532,120 @@ void
 ChangeSize(GLsizei horizontal, GLsizei vertical)
 {
 
-  GLfloat AspectRatio;
-
-  if (vertical == 0)
-    // Zabezpieczenie pzred dzieleniem przez 0
-
-    vertical = 1;
-
-  glViewport(0, 0, horizontal, vertical);
-  // Ustawienie wielkościokna okna urządzenia (Viewport)
-  // W tym przypadku od (0,0) do (horizontal, vertical)
+  pix2anglex = 360.0 / (float)horizontal;
+  pix2angley = 360.0 / (float)vertical;
 
   glMatrixMode(GL_PROJECTION);
-  // Określenie układu współrzędnych obserwatora
 
   glLoadIdentity();
-  // Określenie przestrzeni ograniczającej
 
-  AspectRatio = (GLfloat)horizontal / (GLfloat)vertical;
-  // Wyznaczenie współczynnika proporcji okna
-
-  // Gdy okno na ekranie nie jest kwadratem wymagane jest
-  // określenie okna obserwatora.
-  // Pozwala to zachować właściwe proporcje rysowanego obiektu
-  // Do określenia okna obserwatora służy funkcja glOrtho(...)
+  gluPerspective(70, 1.0, 1.0, 30.0);
 
   if (horizontal <= vertical)
-
-    glOrtho(
-      -100.0, 100.0, -100.0 / AspectRatio, 100.0 / AspectRatio, 1.0, -1.0);
+    glViewport(0, (vertical - horizontal) / 2, horizontal, horizontal);
 
   else
-
-    glOrtho(
-      -100.0 * AspectRatio, 100.0 * AspectRatio, -100.0, 100.0, 1.0, -1.0);
+    glViewport((horizontal - vertical) / 2, 0, vertical, vertical);
 
   glMatrixMode(GL_MODELVIEW);
-  // Określenie układu współrzędnych
 
   glLoadIdentity();
 }
 
-/*************************************************************************************/
+void
+Mouse(int btn, int state, int x, int y)
+{
+  if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+    x_pos_old = x;
+    y_pos_old = y;
+    status = 1;
+  } else {
+    status = 0;
+  }
+}
 
-// Główny punkt wejścia programu. Program działa w trybie konsoli
+void
+Motion(GLsizei x, GLsizei y)
+{
+  delta_x = x - x_pos_old;
+  x_pos_old = x;
+
+  delta_y = y - y_pos_old;
+  y_pos_old = y;
+
+  glutPostRedisplay();
+}
+
+void
+Keyboard(unsigned char key, int x, int y)
+{
+  constexpr double camera_speed = 0.1;
+  constexpr size_t light_speed = 1; // ;]
+
+  switch (key) {
+    case 'l':
+      offset_x += 0.1;
+      break;
+
+    case 'h':
+      offset_x -= 0.1;
+      break;
+
+    case 'k':
+      offset_y += 0.1;
+      break;
+
+    case 'j':
+      offset_y -= 0.1;
+      break;
+
+    case 'f':
+      figure = (figure + 1) % 3;
+      break;
+
+    case 't':
+      tex_active ^= 1;
+      tex_active &= 1;
+
+      if (tex_active == 0) {
+        set_texture(t1);
+      } else {
+        set_texture(t2);
+      }
+
+      break;
+
+    default:
+      fmt::print("Key {}\n", key);
+      break;
+  }
+
+  RenderScene();
+}
 
 int
 main(int argc, char** argv)
-
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-  glutInitWindowSize(1000, 1000);
-  // Ustawienie trybu wyświetlania
-  // GLUT_SINGLE - pojedynczy bufor wyświetlania
-  // GLUT_RGBA - model kolorów RGB
 
-  glutCreateWindow("Drugi program w OpenGL");
-  // Utworzenie okna i określenie treści napisu w nagłówku okna
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(300, 300);
+  glutCreateWindow("Lighting");
 
   glutDisplayFunc(RenderScene);
-  // Określenie, że funkcja RenderScene będzie funkcją zwrotną (callback)
-  // Biblioteka GLUT będzie wywoływała tą funkcję za każdym razem, gdy
-  // trzeba będzie przerysować okno
 
   glutReshapeFunc(ChangeSize);
-  // Dla aktualnego okna ustala funkcję zwrotną odpowiedzialną za
-  // zmiany rozmiaru okna
 
   MyInit();
-  // Funkcja MyInit (zdefiniowana powyżej) wykonuje wszelkie
-  // inicjalizacje konieczneprzed przystąpieniem do renderowania
 
+  glEnable(GL_DEPTH_TEST);
+
+  init_eggpoints();
+
+  glutMouseFunc(Mouse);
+  glutMotionFunc(Motion);
+  glutKeyboardFunc(Keyboard);
   glutMainLoop();
-  // Funkcja uruchamia szkielet biblioteki GLUT
 }
 
 /*************************************************************************************/
